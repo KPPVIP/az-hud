fb922d91704213bfda2b054249849f418c946c36
Guudz <Kilian1702@outlook.com>
2023-02-03 02:35:23 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'ACEZSHOP'
+contact 'discord.gg/KCJDrRWfCe'
+description 'Script de Hud Exclusivo feito para Base ACEZSHOP Exclusiva'
+
+ui_page 'nui/index.html'
+
+client_scripts {
+    '@vrp/lib/utils.lua',
+    'acezshop_cl.lua'
+}
+
+files {
+    'nui/*html',
+    'nui/*js',
+    'nui/*css',
+    'nui/**/*.png',
+    'nui/**/*.ttf'
+}
